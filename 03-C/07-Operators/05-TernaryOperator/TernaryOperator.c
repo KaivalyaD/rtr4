a143f97e462308e9b23c01a03f28c55dd45e705f
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2021-12-05 00:31:37 +0530

    upload 3 correction 1

--- 03-C/07-Operators/05-TernaryOperator/TernaryOperator.c
+++ 03-C/07-Operators/05-TernaryOperator/TernaryOperator.c
@@ -3,20 +3,20 @@
 int main(void)
 {
 	// variable declarations
-	int kvd_a, kvd_b, kvd_p, kvd_q, i_result_01, i_result_02;
-	char ch_result_01, ch_result_02;
+	int kvd_a, kvd_b, kvd_p, kvd_q, kvd_i_result_01, kvd_i_result_02;
+	char kvd_ch_result_01, kvd_ch_result_02;
 
 	// code
 	printf("\n\n");
 
 	kvd_a = 7;
 	kvd_b = 5;
-	ch_result_01 = (kvd_a > kvd_b) ? 'A' : 'B';
-	i_result_01 = (kvd_a > kvd_b) ? kvd_a : kvd_b;
-	printf("ternary operator answer 1: %c and %d\n\n", ch_result_01, i_result_01);
+	kvd_ch_result_01 = (kvd_a > kvd_b) ? 'A' : 'B';
+	kvd_i_result_01 = (kvd_a > kvd_b) ? kvd_a : kvd_b;
+	printf("ternary operator answer 1: %c and %d\n\n", kvd_ch_result_01, kvd_i_result_01);
 
 	kvd_p = kvd_q = 30;
-	ch_result_02 = (kvd_p != kvd_q) ? 'P' : 'Q';
-	i_result_02 = (kvd_p != kvd_q) ? kvd_p : kvd_q;
-	printf("ternary operator answer 2: %c and %d\n\n", ch_result_02, i_result_02);
+	kvd_ch_result_02 = (kvd_p != kvd_q) ? 'P' : 'Q';
+	kvd_i_result_02 = (kvd_p != kvd_q) ? kvd_p : kvd_q;
+	printf("ternary operator answer 2: %c and %d\n\n", kvd_ch_result_02, kvd_i_result_02);
 }
