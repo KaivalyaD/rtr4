fabc8976b3a5f680b9ee3b584e552eb88fc5f178
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2022-03-24 21:56:43 +0530

    added revolution using gluLookAt() and manual transformation for ortho projections

---
+++ 04-Homework/05-ManualTransformation2/OGL.cpp
@@ -0,0 +1,621 @@
+// header files
+#include<windows.h>
+#include<stdlib.h>	// for exit()
+#include<stdio.h>	// for file I/O functions
+#include"OGL.h"
+
+// math headers
+#define _USE_MATH_DEFINES 1
+#include<math.h>	// for PI and trigonometeric functions
+
+// OpenGL headers
+#include<GL/gl.h>
+#include<GL/glu.h>	// graphics library utility
+
+// OpenGL libraries
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
+
+// macros
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+// global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// global variable declarations
+FILE *gpLog = NULL;
+HWND ghwnd = NULL;
+HDC ghdc = NULL;
+HGLRC ghrc = NULL;
+BOOL gbFullScreen = FALSE;
+BOOL gbActiveWindow = FALSE;
+
+// rendering globals
+float angleCube = 0.0f;
+
+// math
+GLfloat identityMatrix[16];
+GLfloat translationMatrix[16];
+GLfloat scaleMatrix[16];
+GLfloat rotationMatrix_X[16];
+GLfloat rotationMatrix_Y[16];
+GLfloat rotationMatrix_Z[16];
+GLfloat orthoTransformationMatrix[16];
+
+// entry-point function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// function prototypes
+	int initialize(void);	// declaring according to the order of use
+	void display(void);
+	void update(void);
+	void uninitialize(void);
+
+	// variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyWindow");
+	BOOL bDone = FALSE;
+	int iRetVal = 0;
+	int cxScreen, cyScreen;
+
+	// code
+	if (fopen_s(&gpLog, "Log.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("fopen_s: failed to open log file"), TEXT("File I/O Error"), MB_OK | MB_ICONERROR);
+		exit(0);
+	}
+	else
+	{
+		fprintf(gpLog, "fopen_s: log file opened successfully\n");
+	}
+
+	// initialization of the wndclass structure
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	// registering the wndclass
+	RegisterClassEx(&wndclass);
+
+	// getting the screen size
+	cxScreen = GetSystemMetrics(SM_CXSCREEN);
+	cyScreen = GetSystemMetrics(SM_CYSCREEN);
+
+	// create the window
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("Manual Transformations: Kaivalya Vishwakumar Deshpande"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		(cxScreen - WIN_WIDTH) / 2,
+		(cyScreen - WIN_HEIGHT) / 2,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ghwnd = hwnd;
+	
+	// initialize
+	iRetVal = initialize();
+	if (iRetVal == -1)
+	{
+		fprintf(gpLog, "ChoosePixelFormat(): failed\n");
+		uninitialize();
+	}
+	else if (iRetVal == -2)
+	{
+		fprintf(gpLog, "SetPixelFormat(): failed\n");
+		uninitialize();
+	}
+	else if (iRetVal == -3)
+	{
+		fprintf(gpLog, "wglCreateContext(): failed\n");
+		uninitialize();
+	}
+	else if (iRetVal == -4)
+	{
+		fprintf(gpLog, "wglMakeCurrent(): failed\n");
+		uninitialize();
+	}
+	else
+	{
+		fprintf(gpLog, "created OpenGL context successfully and made it the current context\n");
+	}
+
+	// show the window
+	ShowWindow(hwnd, iCmdShow);
+
+	// foregrounding and focussing the window
+	SetForegroundWindow(hwnd);	// using ghwnd is obviously fine, but by common sense ghwnd is for global use while we have hwnd locally available in WndProc and here
+	SetFocus(hwnd);
+
+	// game loop
+	while (bDone != TRUE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			if (gbActiveWindow)
+			{
+				// render the scene
+				display();
+
+				// update the scene
+				update();
+			}
+		}
+	}
+
+	// uninitialize
+	uninitialize();
+
+	return (int)msg.wParam;
+}
+
+// callback function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// function prototypes
+	void ToggleFullScreen(void);
+	void resize(int, int);
+
+	// code
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		gbActiveWindow = TRUE;
+		fprintf(gpLog, "window in focus\n");
+		break;
+	case WM_KILLFOCUS:
+		gbActiveWindow = FALSE;
+		fprintf(gpLog, "window out of focus\n");
+		break;
+	case WM_ERASEBKGND:
+		return 0;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case 27:
+			fprintf(gpLog, "destroying after receiving esc\n");
+			DestroyWindow(hwnd);
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case 'F':
+		case 'f':
+			ToggleFullScreen();
+			break;
+		default:
+			break;
+		}
+		break;
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_CLOSE:	// disciplined code: sent as a signal that a window or an application should terminate
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		break;
+	}
+
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+void ToggleFullScreen(void)
+{
+	// variable declarations
+	static DWORD dwStyle;
+	static WINDOWPLACEMENT wp;
+	MONITORINFO mi;
+
+	// code
+	wp.length = sizeof(WINDOWPLACEMENT);
+
+	if (gbFullScreen == FALSE)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			mi.cbSize = sizeof(MONITORINFO);
+
+			if (GetWindowPlacement(ghwnd, &wp) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top,
+					mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+
+			ShowCursor(FALSE);
+			gbFullScreen = TRUE;
+			fprintf(gpLog, "fullscreen mode on\n");
+		}
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wp);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0,
+			SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(TRUE);
+		gbFullScreen = FALSE;
+		fprintf(gpLog, "fullscreen mode off\n");
+	}
+}
+
+int initialize(void)
+{
+	// function prototypes
+	void resize(int, int);
+
+	// variable declarations
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex = 0;
+
+	// code
+	// initialize PIXELFORMATDESCRIPTOR
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;	// R
+	pfd.cGreenBits = 8;	// G
+	pfd.cBlueBits = 8;	// B
+	pfd.cAlphaBits = 8;	// A
+	pfd.cDepthBits = 32;	// 24 is another option
+
+	// get DC
+	ghdc = GetDC(ghwnd);
+
+	// choose pixel format
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0)
+		return -1;
+
+	// set chosen pixel format
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE)
+		return -2;
+
+	// create OpenGL rendering context
+	ghrc = wglCreateContext(ghdc);	// calling my first bridging API
+	if (ghrc == NULL)
+		return -3;
+
+	// make the rendering context the current context
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE)	// the second bridging API
+		return -4;
+
+	// here starts the OpenGL code
+	// clear the screen using black colour
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// changes concerning depth
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	// initialize the unchanging matrix arrays
+	// OpenGL matrices are column major
+	identityMatrix[0] = 1.0f;
+	identityMatrix[1] = 0.0f;
+	identityMatrix[2] = 0.0f;
+	identityMatrix[3] = 0.0f;
+	identityMatrix[4] = 0.0f;
+	identityMatrix[5] = 1.0f;
+	identityMatrix[6] = 0.0f;
+	identityMatrix[7] = 0.0f;
+	identityMatrix[8] = 0.0f;
+	identityMatrix[9] = 0.0f;
+	identityMatrix[10] = 1.0f;
+	identityMatrix[11] = 0.0f;
+	identityMatrix[12] = 0.0f;
+	identityMatrix[13] = 0.0f;
+	identityMatrix[14] = 0.0f;
+	identityMatrix[15] = 1.0f;
+
+	translationMatrix[0] = 1.0f;
+	translationMatrix[1] = 0.0f;
+	translationMatrix[2] = 0.0f;
+	translationMatrix[3] = 0.0f;
+	translationMatrix[4] = 0.0f;
+	translationMatrix[5] = 1.0f;
+	translationMatrix[6] = 0.0f;
+	translationMatrix[7] = 0.0f;
+	translationMatrix[8] = 0.0f;
+	translationMatrix[9] = 0.0f;
+	translationMatrix[10] = 1.0f;
+	translationMatrix[11] = 0.0f;
+	translationMatrix[12] = 0.0f;	// tx = 0
+	translationMatrix[13] = 0.0f;	// ty = 0
+	translationMatrix[14] = -5.0f;	// tz = -5
+	translationMatrix[15] = 1.0f;
+
+	scaleMatrix[0] = 0.75f;	// sx = 75%
+	scaleMatrix[1] = 0.0f;
+	scaleMatrix[2] = 0.0f;
+	scaleMatrix[3] = 0.0f;
+	scaleMatrix[4] = 0.0f;
+	scaleMatrix[5] = 0.75f;	// sy = 75%
+	scaleMatrix[6] = 0.0f;
+	scaleMatrix[7] = 0.0f;
+	scaleMatrix[8] = 0.0f;
+	scaleMatrix[9] = 0.0f;
+	scaleMatrix[10] = 0.75f; // sz = 75%
+	scaleMatrix[11] = 0.0f;
+	scaleMatrix[12] = 0.0f;
+	scaleMatrix[13] = 0.0f;
+	scaleMatrix[14] = 0.0f;
+	scaleMatrix[15] = 1.0f;
+
+	// warm-up resize call
+	resize(WIN_WIDTH, WIN_HEIGHT);
+
+	return 0;
+}
+
+void resize(int width, int height)
+{
+	// variable declarations
+	GLfloat r, l, t, b, n, f;
+
+	// code
+	if (height == 0)
+		height = 1;	// to prevent a divide by zero when calculating the width/height ratio
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	if (width >= height)
+	{
+		l = -5.0f;
+		r = 5.0f;
+		b = -5.0f * ((GLfloat)height / (GLfloat)width);
+		t = 5.0f * ((GLfloat)height / (GLfloat)width);
+		n = 5.0f;
+		f = -5.0f;
+	}
+	else
+	{
+		l = -5.0f * ((GLfloat)width / (GLfloat)height);
+		r = 5.0f * ((GLfloat)width / (GLfloat)height);
+		b = -5.0f;
+		t = 5.0f;
+		n = 5.0f;
+		f = -5.0f;
+	}
+	
+	orthoTransformationMatrix[0] = 2.0f / (GLfloat)(r - l);
+	orthoTransformationMatrix[1] = 0.0f;
+	orthoTransformationMatrix[2] = 0.0f;
+	orthoTransformationMatrix[3] = 0.0f;
+	orthoTransformationMatrix[4] = 0.0f;
+	orthoTransformationMatrix[5] = 2.0f / (GLfloat)(t - b);
+	orthoTransformationMatrix[6] = 0.0f;
+	orthoTransformationMatrix[7] = 0.0f;
+	orthoTransformationMatrix[8] = 0.0f;
+	orthoTransformationMatrix[9] = 0.0f;
+	orthoTransformationMatrix[10] = 2.0f / (GLfloat)(f - n);
+	orthoTransformationMatrix[11] = 0.0f;
+	orthoTransformationMatrix[12] = (GLfloat)(r + l) / (GLfloat)(r - l);
+	orthoTransformationMatrix[13] = (GLfloat)(t + b) / (GLfloat)(t - b);
+	orthoTransformationMatrix[14] = (GLfloat)(f + n) / (GLfloat)(f - n);
+	orthoTransformationMatrix[15] = 1.0f;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadMatrixf(identityMatrix);
+	glMultMatrixf(orthoTransformationMatrix);
+}
+
+void display(void)
+{
+	// code
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	
+	glLoadMatrixf(identityMatrix);	// glLoadIdentity();
+	glMultMatrixf(translationMatrix);	// glTranslatef(0.0f, 0.0f, -5.0f);
+	glMultMatrixf(scaleMatrix);	// glScalef(0.75f, 0.75f, 0.75f);
+	
+	// converting degrees to radians
+	float angle = angleCube * (M_PI / 180.0f);
+
+	// about X
+	rotationMatrix_X[0] = 1.0f;
+	rotationMatrix_X[1] = 0.0f;
+	rotationMatrix_X[2] = 0.0f;
+	rotationMatrix_X[3] = 0.0f;
+	rotationMatrix_X[4] = 0.0f;
+	rotationMatrix_X[5] = cos(angle);
+	rotationMatrix_X[6] = sin(angle);
+	rotationMatrix_X[7] = 0.0f;
+	rotationMatrix_X[8] = 0.0f;
+	rotationMatrix_X[9] = -sin(angle);
+	rotationMatrix_X[10] = cos(angle);
+	rotationMatrix_X[11] = 0.0f;
+	rotationMatrix_X[12] = 0.0f;
+	rotationMatrix_X[13] = 0.0f;
+	rotationMatrix_X[14] = 0.0f;
+	rotationMatrix_X[15] = 1.0f;
+
+	// about Y
+	rotationMatrix_Y[0] = cos(angle);
+	rotationMatrix_Y[1] = 0.0f;
+	rotationMatrix_Y[2] = -sin(angle);
+	rotationMatrix_Y[3] = 0.0f;
+	rotationMatrix_Y[4] = 0.0f;
+	rotationMatrix_Y[5] = 1.0f;
+	rotationMatrix_Y[6] = 0.0f;
+	rotationMatrix_Y[7] = 0.0f;
+	rotationMatrix_Y[8] = sin(angle);
+	rotationMatrix_Y[9] = 0.0f;
+	rotationMatrix_Y[10] = cos(angle);
+	rotationMatrix_Y[11] = 0.0f;
+	rotationMatrix_Y[12] = 0.0f;
+	rotationMatrix_Y[13] = 0.0f;
+	rotationMatrix_Y[14] = 0.0f;
+	rotationMatrix_Y[15] = 1.0f;
+
+	// about Z
+	rotationMatrix_Z[0] = cos(angle);
+	rotationMatrix_Z[1] = sin(angle);
+	rotationMatrix_Z[2] = 0.0f;
+	rotationMatrix_Z[3] = 0.0f;
+	rotationMatrix_Z[4] = -sin(angle);
+	rotationMatrix_Z[5] = cos(angle);
+	rotationMatrix_Z[6] = 0.0f;
+	rotationMatrix_Z[7] = 0.0f;
+	rotationMatrix_Z[8] = 0.0f;
+	rotationMatrix_Z[9] = 0.0f;
+	rotationMatrix_Z[10] = 1.0f;
+	rotationMatrix_Z[11] = 0.0f;
+	rotationMatrix_Z[12] = 0.0f;
+	rotationMatrix_Z[13] = 0.0f;
+	rotationMatrix_Z[14] = 0.0f;
+	rotationMatrix_Z[15] = 1.0f;
+
+	// glRotatef(angleCube, 1.0f, 1.0f, 1.0f);
+	glMultMatrixf(rotationMatrix_X);
+	glMultMatrixf(rotationMatrix_Y);
+	glMultMatrixf(rotationMatrix_Z);
+
+	glBegin(GL_QUADS);
+	{
+		// front face
+		glColor3f(1.0f, 0.0f, 0.0f);
+
+		glVertex3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+
+		// right face
+		glColor3f(0.0f, 1.0f, 0.0f);
+
+		glVertex3f(1.0f, 1.0f, -1.0f);
+		glVertex3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
+
+		// rear face
+		glColor3f(0.0f, 0.0f, 1.0f);
+
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(1.0f, 1.0f, -1.0f);
+		glVertex3f(1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+
+		// left face
+		glColor3f(0.0f, 1.0f, 1.0f);
+
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+
+		// top face
+		glColor3f(1.0f, 0.0f, 1.0f);
+
+		glVertex3f(1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, 1.0f, -1.0f);
+		glVertex3f(-1.0f, 1.0f, 1.0f);
+		glVertex3f(1.0f, 1.0f, 1.0f);
+
+		// bottom face
+		glColor3f(1.0f, 1.0f, 0.0f);
+
+		glVertex3f(1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, -1.0f);
+		glVertex3f(-1.0f, -1.0f, 1.0f);
+		glVertex3f(1.0f, -1.0f, 1.0f);
+	}
+	glEnd();
+
+	SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+	// code
+	angleCube = angleCube + 0.05f;
+	if (angleCube >= 360.0f)
+		angleCube = angleCube - 360.0f;
+}
+
+void uninitialize(void)
+{
+	// function prototypes
+	void ToggleFullScreen(void);
+
+	// code
+	if (gbFullScreen)
+	{
+		ToggleFullScreen();
+	}
+
+	if (wglGetCurrentContext() == ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	if (ghrc)
+	{
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+	}
+
+	if (ghdc)
+	{
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (ghwnd)
+	{
+		DestroyWindow(ghwnd);	// if unitialize() was not called from WM_DESTROY
+		ghwnd = NULL;
+	}
+
+	if (gpLog)
+	{
+		fprintf(gpLog, "fclose: closing log file\n");
+		fclose(gpLog);
+		gpLog = NULL;
+	}
+}
