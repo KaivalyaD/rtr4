2129c351a9d12f8de30c99dfae358122cf6c9e33
KaivalyaD <kaivalya.v.deshpande@gmail.com>
2023-03-31 23:36:15 +0530

    added all android assignments except framebuffer

---
+++ 01-OpenGL/02-PP/04-Android/10-OpenGLES/16-TessellationShaders/app/src/main/java/com/kayd/window/GLESView.java
@@ -0,0 +1,532 @@
+package com.kayd.window;
+
+import android.opengl.GLSurfaceView;  // for GLSurfaceView
+import javax.microedition.khronos.opengles.GL10;  // for GL10
+
+import javax.microedition.khronos.egl.EGLConfig;  // for EGLConfig
+
+// OpenGL ES 3.2 namespace
+import android.opengl.GLES32;
+
+import android.content.Context;  // for Context
+
+// Event related packages
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+// Buffer related packages (nio = Native I/O or Non-blocking I/O)
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+// Matrix package
+import android.opengl.Matrix;
+
+public class GLESView extends GLSurfaceView implements OnDoubleTapListener, OnGestureListener, GLSurfaceView.Renderer {
+    public static final int KVD_ATTRIBUTE_POSITION = 0;
+    public static final int KVD_ATTRIBUTE_COLOR = 1;
+    public static final int KVD_ATTRIBUTE_NORMAL = 2;
+    public static final int KVD_ATTRIBUTE_TEXTURE0 = 3;
+    
+    private GestureDetector gestureDetector;
+    private Context context;
+
+    private int[] vao = new int[1];
+    private int[] vbo = new int[1];
+    private int mvpMatrixUniform;
+    private int numberOfSegmentsUniform;
+    private int numberOfStripsUniform;
+    private int lineColorUniform;
+    private int shaderProgramObject;
+
+    private int numberOfSegments;
+    private boolean isSegmentCountIncreasing;
+    private float[] perspectiveProjectionMatrix = new float[16];
+
+    public GLESView(Context _context) {
+        super(_context);
+        
+        context = _context;
+
+        // use latest OpenGL ES version 3.2, based on EGL (Embedded Graphics Library) at impl. level (NDK)
+        setEGLContextClientVersion(3);
+        
+        // use this class as the renderer
+        setRenderer(this);
+
+        // when view is dirty, invalidate it
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+        gestureDetector = new GestureDetector(context, this, null, false);
+        gestureDetector.setOnDoubleTapListener(this);
+    }
+
+    /* implementing GLSurfaceView.Renderer */
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        System.out.println("KVD: OpenGLES version: " + gl.glGetString(GL10.GL_VERSION));
+        System.out.println("KVD: Renderer: " + gl.glGetString(GL10.GL_RENDERER));
+        System.out.println("KVD: GLSL version: " + gl.glGetString(GLES32.GL_SHADING_LANGUAGE_VERSION));
+
+        initialize();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        resize(width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        /*
+         * consider as the game loop
+         */
+        
+        update();
+        render();
+    }
+
+    /* of this view itself */
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if(!gestureDetector.onTouchEvent(e))
+            super.onTouchEvent(e);
+
+        return true;
+    }
+
+    /* implementing OnDoubleTapListener */
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        /*
+         * this trigger shall toggle lighting
+         */
+
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        /* 
+         * onDoubleTapEvent() may or may not carry forward to super,
+         * but onDoubleTap() always does; that's the reason we have
+         * handled it and not this
+         */
+
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        if(isSegmentCountIncreasing) {
+            numberOfSegments += 1;
+            if(numberOfSegments == 30)
+                isSegmentCountIncreasing = false;
+        }
+        else {
+            numberOfSegments -= 1;
+            if(numberOfSegments == 1)
+                isSegmentCountIncreasing = true;
+        }
+        return true;
+    }
+
+    /* implementing OnGestureListener */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent eIn, MotionEvent eOut, float velocityX, float velocityY) {
+        
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        /*
+         * this trigger shall toggle textures
+         */
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent eIn, MotionEvent eOut, float distanceX, float distanceY) {
+        uninitialize();
+        System.exit(0);
+
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+    
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+
+        return true;
+    }
+
+    /* original methods of this class */
+    private void initialize() {
+        int[] status = new int[1];
+        int[] infoLogLength = new int[1];
+        String infoLog = null;
+
+        // vertex shader
+        final String vertexShaderSourceCode = String.format(
+            "#version 320 es\n" +
+            "\n" +
+            "in vec2 a_position;\n" +
+            "\n" +
+            "void main(void)\n" +
+            "{\n" +
+                "gl_Position = vec4(a_position, 0.0f, 1.0f);\n" +
+            "}\n"
+        );
+
+        int vertexShaderObject = GLES32.glCreateShader(GLES32.GL_VERTEX_SHADER);
+        GLES32.glShaderSource(vertexShaderObject, vertexShaderSourceCode);
+        GLES32.glCompileShader(vertexShaderObject);
+        GLES32.glGetShaderiv(vertexShaderObject, GLES32.GL_COMPILE_STATUS, status, 0);
+        if(status[0] == GLES32.GL_FALSE) {
+            System.out.println("KVD: *** vertex shader compilation errors ***");
+
+            GLES32.glGetShaderiv(vertexShaderObject, GLES32.GL_INFO_LOG_LENGTH, infoLogLength, 0);
+            if(infoLogLength[0] > 0) {
+                infoLog = GLES32.glGetShaderInfoLog(vertexShaderObject);
+                System.out.println("KVD: vertex shader compilation log (" + infoLogLength[0] + " bytes):");
+                System.out.println(infoLog);
+            }
+            else {
+                System.out.println("KVD: there is nothing to print");
+            }
+
+            uninitialize();
+            System.exit(0);
+        }
+
+        status[0] = 0;
+        infoLogLength[0] = 0;
+        infoLog = null;
+
+        // tessellation control shader
+        final String tessellationControlShaderSourceCode = String.format(
+            "#version 320 es\n" +
+            "\n" +
+            "layout(vertices=4) out;\n" +
+            "\n" +
+            "uniform int u_numberOfSegments;\n" +
+            "uniform int u_numberOfStrips;\n" +
+            "\n" +
+            "void main(void)\n" +
+            "{\n" +
+            "   gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;\n" +
+            "   gl_TessLevelOuter[0] = float(u_numberOfStrips);\n" +
+            "   gl_TessLevelOuter[1] = float(u_numberOfSegments);\n" +
+            "}\n"
+        );
+
+        int tessellationControlShaderObject = GLES32.glCreateShader(GLES32.GL_TESS_CONTROL_SHADER);
+        GLES32.glShaderSource(tessellationControlShaderObject, tessellationControlShaderSourceCode);
+        GLES32.glCompileShader(tessellationControlShaderObject);
+        GLES32.glGetShaderiv(tessellationControlShaderObject, GLES32.GL_COMPILE_STATUS, status, 0);
+        if(status[0] == GLES32.GL_FALSE) {
+            System.out.println("KVD: *** tessellation control shader compilation errors ***");
+
+            GLES32.glGetShaderiv(tessellationControlShaderObject, GLES32.GL_INFO_LOG_LENGTH, infoLogLength, 0);
+            if(infoLogLength[0] > 0) {
+                infoLog = GLES32.glGetShaderInfoLog(tessellationControlShaderObject);
+                System.out.println("KVD: tessellation control shader compilation log (" + infoLogLength[0] + " bytes):");
+                System.out.println(infoLog);
+            }
+            else {
+                System.out.println("KVD: there is nothing to print");
+            }
+
+            uninitialize();
+            System.exit(0);
+        }
+
+        status[0] = 0;
+        infoLogLength[0] = 0;
+        infoLog = null;
+        
+        // tessellation evaluation shader
+        final String tessellationEvaluationShaderSourceCode = String.format(
+            "#version 320 es\n" +
+            "\n" +
+            "layout(isolines) in;\n" +
+            "\n" +
+            "uniform mat4 u_mvpMatrix;\n" +
+            "\n" +
+            "void main(void)\n" +
+            "{\n" +
+            "   vec3 P0 = gl_in[0].gl_Position.xyz;\n" +
+            "   vec3 P1 = gl_in[1].gl_Position.xyz;\n" +
+            "   vec3 P2 = gl_in[2].gl_Position.xyz;\n" +
+            "   vec3 P3 = gl_in[3].gl_Position.xyz;\n" +
+            "   \n" +
+            "   float u = gl_TessCoord.x;\n" +
+            "   \n" +
+            "   vec3 P = P0 * (1.0 - u) * (1.0 - u) * (1.0 - u) + " +
+                    "P1 * 3.0 * u * (1.0 - u) * (1.0 - u) + " +
+                    "P2 * 3.0 * u * u * (1.0 - u) + " +
+                    "P3 * u * u * u;\n" +
+            "   \n" +
+            "   gl_Position = u_mvpMatrix * vec4(P, 1.0);\n" +
+            "}\n"  /* declare u as a double if using this shader in vulkan */
+        );
+
+        int tessellationEvaluationShaderObject = GLES32.glCreateShader(GLES32.GL_TESS_EVALUATION_SHADER);
+        GLES32.glShaderSource(tessellationEvaluationShaderObject, tessellationEvaluationShaderSourceCode);
+        GLES32.glCompileShader(tessellationEvaluationShaderObject);
+        GLES32.glGetShaderiv(tessellationEvaluationShaderObject, GLES32.GL_COMPILE_STATUS, status, 0);
+        if(status[0] == GLES32.GL_FALSE) {
+            System.out.println("KVD: *** tessellation evaluation shader compilation errors ***");
+
+            GLES32.glGetShaderiv(tessellationEvaluationShaderObject, GLES32.GL_INFO_LOG_LENGTH, infoLogLength, 0);
+            if(infoLogLength[0] > 0) {
+                infoLog = GLES32.glGetShaderInfoLog(tessellationEvaluationShaderObject);
+                System.out.println("KVD: tessellation evaluation shader compilation log (" + infoLogLength[0] + " bytes):");
+                System.out.println(infoLog);
+            }
+            else {
+                System.out.println("KVD: there is nothing to print");
+            }
+
+            uninitialize();
+            System.exit(0);
+        }
+
+        status[0] = 0;
+        infoLogLength[0] = 0;
+        infoLog = null;
+
+        // fragment shader
+        final String fragmentShaderSourceCode = String.format(
+            "#version 320 es\n" +
+            "precision highp float;\n" +
+            "\n" +
+            "uniform vec4 u_lineColor;\n" +
+            "\n" +
+            "out vec4 FragColor;\n" +
+            "\n" +
+            "void main(void)\n" +
+            "{\n" +
+                "FragColor = u_lineColor;\n" +
+            "}\n"
+        );
+
+        int fragmentShaderObject = GLES32.glCreateShader(GLES32.GL_FRAGMENT_SHADER);
+        GLES32.glShaderSource(fragmentShaderObject, fragmentShaderSourceCode);
+        GLES32.glCompileShader(fragmentShaderObject);
+        GLES32.glGetShaderiv(fragmentShaderObject, GLES32.GL_COMPILE_STATUS, status, 0);
+        if(status[0] == GLES32.GL_FALSE) {
+            System.out.println("KVD: *** fragment shader compilation errors ***");
+
+            GLES32.glGetShaderiv(fragmentShaderObject, GLES32.GL_INFO_LOG_LENGTH, infoLogLength, 0);
+            if(infoLogLength[0] > 0) {
+                infoLog = GLES32.glGetShaderInfoLog(fragmentShaderObject);
+                System.out.println("KVD: fragment shader compilation log (" + infoLogLength[0] + " bytes):");
+                System.out.println(infoLog);
+            }
+            else {
+                System.out.println("KVD: there is nothing to print");
+            }
+
+            uninitialize();
+            System.exit(0);
+        }
+        
+        status[0] = 0;
+        infoLogLength[0] = 0;
+        infoLog = null;
+
+        shaderProgramObject = GLES32.glCreateProgram();
+        GLES32.glAttachShader(shaderProgramObject, vertexShaderObject);
+        GLES32.glAttachShader(shaderProgramObject, tessellationControlShaderObject);
+        GLES32.glAttachShader(shaderProgramObject, tessellationEvaluationShaderObject);
+        GLES32.glAttachShader(shaderProgramObject, fragmentShaderObject);
+        GLES32.glBindAttribLocation(shaderProgramObject, GLESView.KVD_ATTRIBUTE_POSITION, "a_position");
+        GLES32.glLinkProgram(shaderProgramObject);
+        GLES32.glGetProgramiv(shaderProgramObject, GLES32.GL_LINK_STATUS, status, 0);
+        if(status[0] == GLES32.GL_FALSE) {
+            System.out.println("KVD: *** there were linking errors ***");
+
+            GLES32.glGetProgramiv(shaderProgramObject, GLES32.GL_INFO_LOG_LENGTH, infoLogLength, 0);
+            if(infoLogLength[0] > 0) {
+                infoLog = GLES32.glGetProgramInfoLog(shaderProgramObject);
+                System.out.println("KVD: link time info log (" + infoLogLength[0] + " bytes):");
+                System.out.println(infoLog);
+            }
+            else {
+                System.out.println("KVD: there is nothing to print");
+            }
+
+            uninitialize();
+            System.exit(0);
+        }
+
+        // get all uniform locations
+        mvpMatrixUniform = GLES32.glGetUniformLocation(shaderProgramObject, "u_mvpMatrix");
+        numberOfSegmentsUniform = GLES32.glGetUniformLocation(shaderProgramObject, "u_numberOfSegments");
+        numberOfStripsUniform = GLES32.glGetUniformLocation(shaderProgramObject, "u_numberOfStrips");
+        lineColorUniform = GLES32.glGetUniformLocation(shaderProgramObject, "u_lineColor");
+
+        // vao
+        GLES32.glGenVertexArrays(1, vao, 0);
+        GLES32.glBindVertexArray(vao[0]);
+        {
+            // positions vbo
+            GLES32.glGenBuffers(1, vbo, 0);
+            GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER, vbo[0]);
+            {
+                final float[] vertexPositions = {
+                   -1.0f, -1.0f,
+                   -0.5f,  1.0f,
+                    0.5f, -1.0f,
+                    1.0f,  1.0f
+                };
+                
+                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertexPositions.length * 4);
+                byteBuffer.order(ByteOrder.nativeOrder());
+
+                FloatBuffer vertexPositionsBuffer = byteBuffer.asFloatBuffer();
+                vertexPositionsBuffer.put(vertexPositions);
+                vertexPositionsBuffer.position(0);
+
+                GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER, vertexPositions.length * 4, vertexPositionsBuffer, GLES32.GL_STATIC_DRAW);
+                GLES32.glVertexAttribPointer(GLESView.KVD_ATTRIBUTE_POSITION, 2, GLES32.GL_FLOAT, false, 0, 0);
+                GLES32.glEnableVertexAttribArray(GLESView.KVD_ATTRIBUTE_POSITION);
+            }
+            GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER, 0);
+        }
+        GLES32.glBindVertexArray(0);
+
+        // regular OpenGL setup
+        GLES32.glClearDepthf(1.0f);
+        GLES32.glEnable(GLES32.GL_DEPTH_TEST);
+        GLES32.glDepthFunc(GLES32.GL_LEQUAL);
+
+        // enable culling for better performance on es
+        GLES32.glCullFace(GLES32.GL_BACK);
+        GLES32.glEnable(GLES32.GL_CULL_FACE);
+
+        // initialize projection matrix
+        Matrix.setIdentityM(perspectiveProjectionMatrix, 0);
+
+        // start with numberOfSegments = 1
+        numberOfSegments = 1;
+        isSegmentCountIncreasing = true;
+
+        GLES32.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    private void resize(int width, int height) {
+        height = (height <= 0 ? 1 : height);
+
+        GLES32.glViewport(0, 0, width, height);
+
+        float aspectRatio = (float)width / (float)height;
+        Matrix.perspectiveM(
+            perspectiveProjectionMatrix, 0,
+            45.0f,
+            aspectRatio,
+            0.1f,
+            100.0f
+        );
+    }
+
+    private void render() {
+        float[] translationMatrix = new float[16];
+        float[] modelViewMatrix = new float[16];
+        float[] modelViewProjectionMatrix = new float[16];
+
+        Matrix.setIdentityM(translationMatrix, 0);
+        Matrix.setIdentityM(modelViewMatrix, 0);
+        Matrix.setIdentityM(modelViewProjectionMatrix, 0);
+
+        GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT | GLES32.GL_DEPTH_BUFFER_BIT);
+
+        GLES32.glUseProgram(shaderProgramObject);
+        {
+            Matrix.translateM(translationMatrix, 0, 0.0f, 0.0f, -3.0f);
+            Matrix.multiplyMM(
+                modelViewMatrix, 0,
+                translationMatrix, 0,
+                modelViewMatrix, 0
+            );
+
+            Matrix.multiplyMM(
+                modelViewProjectionMatrix, 0,
+                perspectiveProjectionMatrix, 0,
+                modelViewMatrix, 0
+            );
+            
+            GLES32.glUniformMatrix4fv(mvpMatrixUniform, 1, false, modelViewProjectionMatrix, 0);
+            GLES32.glUniform1i(numberOfSegmentsUniform, numberOfSegments);
+            GLES32.glUniform1i(numberOfStripsUniform, 1);
+            if(numberOfSegments == 30)
+                GLES32.glUniform4f(lineColorUniform, 0.0f, 1.0f, 0.0f, 1.0f);
+            else
+                GLES32.glUniform4f(lineColorUniform, 1.0f, 0.0f, 0.0f, 1.0f);
+
+            GLES32.glBindVertexArray(vao[0]);
+                GLES32.glPatchParameteri(GLES32.GL_PATCH_VERTICES, 4);
+                GLES32.glDrawArrays(GLES32.GL_PATCHES, 0, 4);
+            GLES32.glBindVertexArray(0);
+        }
+        GLES32.glUseProgram(0);
+
+        requestRender();  // swapping buffers
+    }
+
+    private void update() {
+        /* no update needed */
+    }
+
+    private void uninitialize() {
+        if(vbo[0] > 0) {
+            GLES32.glDeleteBuffers(1, vbo, 0);
+            vbo[0] = 0;
+        }
+        
+        if(vao[0] > 0) {
+            GLES32.glDeleteVertexArrays(1, vao, 0);
+            vao[0] = 0;
+        }
+
+        if(shaderProgramObject > 0) {
+            int[] retval = new int[1];
+
+            GLES32.glUseProgram(shaderProgramObject);
+            {
+                GLES32.glGetProgramiv(shaderProgramObject, GLES32.GL_ATTACHED_SHADERS, retval, 0);
+                if(retval[0] > 0) {
+                    int numAttachedShaders = retval[0];
+                    int[] shaderObjects = new int[numAttachedShaders];
+                    
+                    GLES32.glGetAttachedShaders(shaderProgramObject, numAttachedShaders, retval, 0, shaderObjects, 0);
+                    for(int i = 0; i < numAttachedShaders; i++) {
+                        GLES32.glDetachShader(shaderProgramObject, shaderObjects[i]);
+                        GLES32.glDeleteShader(shaderObjects[i]);
+                        shaderObjects[i] = 0;
+                    }
+
+                    System.out.println("KVD: detached and deleted " + numAttachedShaders + " shader objects");
+                }
+            }
+            GLES32.glUseProgram(0);
+
+            GLES32.glDeleteProgram(shaderProgramObject);
+            shaderProgramObject = 0;
+            System.out.println("KVD: deleted shader program object");
+        }
+    }
+}
